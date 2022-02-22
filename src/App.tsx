0ce2eaef58b6f79811fc0e551dc4b9a582df5c7f
giovannabdos <78513870+giovannabdos@users.noreply.github.com>
2022-02-21 21:46:38 -0300

    finalizando projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Table from "react-bootstrap/Table";
 import Alert from "react-bootstrap/Alert";
 import Dropdown from "react-bootstrap/Dropdown";
@@ -7,70 +7,137 @@ import NavLink from "react-bootstrap/NavLink";
 import Form from "react-bootstrap/Form";
 import { BsThreeDotsVertical, BsFillPencilFill, BsX } from "react-icons/bs";
 
+interface Geo {
+  lat: string;
+  lng: string;
+}
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+}
+
 function App() {
+  const [users, setUsers] = useState<Array<User>>([]);
+  const [usernameColumn, setUsernameColumn] = useState<boolean>(true);
+  const [emailColumn, setEmailColumn] = useState<boolean>(true);
+  const [clienteColumn, setClienteColumn] = useState<boolean>(true);
+  const [perfilColumn, setPerfilColumn] = useState<boolean>(true);
+  const [padrao, setPadrao] = useState<boolean>(true);
+
+  useEffect(() => {
+    fetchUsers();
+  }, [padrao]);
+
+  const fetchUsers = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((result) => {
+        setUsers(result.slice(0, padrao ? 5 : 10));
+      })
+      .catch((err) => {
+        console.error("Failed retrieving information", err);
+      });
+  };
+
+  const deleteUser = (user: User) => {
+    setUsers(users.filter((u) => u.id != user.id));
+  };
+
   return (
     <main>
-      <Table striped hover borderless>
+      <Table striped hover borderless className="users-table">
         <thead>
           <tr>
-            <th>Usuário</th>
-            <th>Email</th>
-            <th>Cliente</th>
-            <th>Perfil de acesso</th>
+            {usernameColumn && <th>Usuário</th>}
+            {emailColumn && <th>Email</th>}
+            {clienteColumn && <th>Cliente</th>}
+            {perfilColumn && <th>Perfil de acesso</th>}
             <th>
               <Dropdown as={NavItem}>
-                <Dropdown.Toggle as={NavLink}>
-                  <BsThreeDotsVertical />
+                <Dropdown.Toggle as={NavLink} className="threeDots">
+                  <BsThreeDotsVertical size={13} color="#3f76ad" />
                 </Dropdown.Toggle>
 
                 <Dropdown.Menu>
                   <Dropdown.Header>Linhas por página</Dropdown.Header>
-                  <Form.Check type="checkbox" label="Padrão" />
-                  <Form.Check type="checkbox" label="50 Linhas" />
+                  <Form.Check
+                    type="checkbox"
+                    label="Padrão"
+                    checked={padrao}
+                    onChange={() => setPadrao(!padrao)}
+                  />
+                  <Form.Check
+                    type="checkbox"
+                    label="10 Linhas"
+                    checked={!padrao}
+                    onChange={() => setPadrao(!padrao)}
+                  />
                   <Dropdown.Divider />
                   <Dropdown.Header>Colunas</Dropdown.Header>
-                  <Form.Check type="checkbox" label="Usuário" />
-                  <Form.Check type="checkbox" label="E-mail" />
-                  <Form.Check type="checkbox" label="Cliente" />
-                  <Form.Check type="checkbox" label="Perfil de acesso" />
+                  <Form.Check
+                    type="checkbox"
+                    label="Usuário"
+                    checked={usernameColumn}
+                    onChange={() => setUsernameColumn(!usernameColumn)}
+                  />
+                  <Form.Check
+                    type="checkbox"
+                    label="E-mail"
+                    checked={emailColumn}
+                    onChange={() => setEmailColumn(!emailColumn)}
+                  />
+                  <Form.Check
+                    type="checkbox"
+                    label="Cliente"
+                    checked={clienteColumn}
+                    onChange={() => setClienteColumn(!clienteColumn)}
+                  />
+                  <Form.Check
+                    type="checkbox"
+                    label="Perfil de acesso"
+                    checked={perfilColumn}
+                    onChange={() => setPerfilColumn(!perfilColumn)}
+                  />
                 </Dropdown.Menu>
               </Dropdown>
             </th>
           </tr>
         </thead>
         <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              {usernameColumn && <td>{user.name}</td>}
+              {emailColumn && <td>{user.email}</td>}
+              {clienteColumn && <td>{user.address.city}</td>}
+              {perfilColumn && (
+                <td>
+                  <Alert variant={"primary"}>{user.username}</Alert>
+                </td>
+              )}
+              <td>
+                <BsFillPencilFill color="#6e97c0" />{" "}
+                <BsX
+                  onClick={() => deleteUser(user)}
+                  color="red"
+                  size={18}
+                  className="delete"
+                />
+              </td>
+            </tr>
+          ))}
           <tr>
-            <td>Giovanna Sacramento</td>
-            <td>gile-2008@hotmail.com</td>
-            <td>Opleplan</td>
-            <td>
-              <Alert variant={"primary"}>@giovanna</Alert>
-            </td>
-            <td>
-              <BsFillPencilFill /> <BsX />
-            </td>
-          </tr>
-          <tr>
-            <td>Wellington</td>
-            <td>Wellington@hotmail.com</td>
-            <td>Opleplan</td>
-            <td>
-              <Alert variant={"primary"}>@wellington</Alert>
-            </td>
-            <td>
-              <BsFillPencilFill /> <BsX />
-            </td>
-          </tr>
-          <tr>
-            <td>Adriana</td>
-            <td>adriana@hotmail.com</td>
-            <td>Opleplan</td>
-            <td>
-              <Alert variant={"primary"}>@adriana</Alert>
-            </td>
-            <td>
-              <BsFillPencilFill /> <BsX />
-            </td>
+            <td colSpan={5} className="last-line" />
           </tr>
         </tbody>
       </Table>
